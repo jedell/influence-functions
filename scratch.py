2fa4e787aa3636094277f28308e9ff53343a1079
joe <jre2016@gmail.com>
2023-11-17 08:41:58 -0500

    ihvp

--- scratch.py
+++ scratch.py
@@ -20,14 +20,12 @@ train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 # 1. get a_l-1, use forward hook to save input to a layer l during the forward pass
 layer_inputs = {}
 
-def forward_hook_fn(module, input, output):
-    if isinstance(module, MLP):
-        print("mlp in", input[0].shape)
-        print("mlp out", output[0].shape)
-    layer_inputs[module] = torch.cat([
-            input[0],
-            torch.ones((input[0].shape[0], input[0].shape[1], 1)).to(input[0].device),
-       ], dim=-1).clone()
+def forward_hook_fn(module, input):
+    if isinstance(module, nn.Linear):
+        layer_inputs[module] = torch.cat([
+                input[0],
+                torch.ones((input[0].shape[0], input[0].shape[1], 1)).to(input[0].device),
+        ], dim=-1).clone()
 
 # 2. get grad_loss, gradients of loss wrt output of linear transformation W_l a_l-1
 #    using a backward hook on the linear layer that saves the gradient wrt the linear layer's output
@@ -35,18 +33,21 @@ def forward_hook_fn(module, input, output):
 layer_grads = {}
 
 def back_hook_fn(module, grad_input, grad_output):
-    if isinstance(module, MLP):
-        print("mlp grad out", grad_output)
-    layer_grads[module] = grad_output[0]
+    if isinstance(module, nn.Linear):
+        layer_grads[module] = grad_output[0]
+
+linear_layers = []
 
 def compute_ekfac_factors(model: nn.Module, train_dataset: Dataset):
 
     kfac_input_covs = []
     kfac_grad_covs = []
 
-    for name, module in model.named_modules():
+
+    for _, module in model.named_modules():
+        # TODO register miniGPT MLP, first linear layer???
         if isinstance(module, nn.Linear):
-            module.register_forward_hook(forward_hook_fn)
+            module.register_forward_pre_hook(forward_hook_fn)
             module.register_full_backward_hook(back_hook_fn)
 
             kfac_input_covs.append(
@@ -56,7 +57,11 @@ def compute_ekfac_factors(model: nn.Module, train_dataset: Dataset):
             kfac_grad_covs.append(
                 torch.zeros((module.weight.shape[0], module.weight.shape[0])).to(device)
             )
-    grads = [[] for _ in range(len([module for _, module in model.named_modules() if isinstance(module, nn.Linear)]))]
+
+            # grab linear layers
+            linear_layers.append(module)
+
+    grads = [[] for _ in range(len(linear_layers))]
     tot = 0
     for X, Y in train_dataset:
 
@@ -72,33 +77,26 @@ def compute_ekfac_factors(model: nn.Module, train_dataset: Dataset):
         logits, loss = model(x_ids, y_ids)
 
         # for each linear/mlp block, get a_l-1
-        i = 0
-        for named_modules in model.named_modules():
-            name, module = named_modules
-            if isinstance(module, nn.Linear):
-                a_l_1 = layer_inputs[module]
-                input_covs = torch.einsum("...ti,...tj->tij", a_l_1, a_l_1)
-                kfac_input_covs[i] += input_covs.mean(dim=0)
-                i+=1
+        for i, module in enumerate(linear_layers):
+            a_l_1 = layer_inputs[module]
+            input_covs = torch.einsum("...ti,...tj->tij", a_l_1, a_l_1)
+            kfac_input_covs[i] += input_covs.mean(dim=0)
 
         loss.backward()
 
         # grad_loss = layer_grads[module]
-        i = 0
-        for named_modules in model.named_modules():
-            name, module = named_modules
-            if isinstance(module, nn.Linear):
-                d_s_l = layer_grads[module]
-                grad_cov = torch.einsum("...ti,...tj->tij", d_s_l, d_s_l)
-                # TODO 
-                # RuntimeError: The size of tensor a (24) must match the size of tensor b (8) at non-singleton dimension 1
-                kfac_grad_covs[i] += grad_cov.mean(dim=0)
-                
-                # gradient of the loss wrt the weights
-                w_grad = module.weight.grad
-                b_grad = module.bias.grad.unsqueeze(-1)
-                full_grad = torch.cat([w_grad, b_grad], dim=-1)
-                grads[i].append(full_grad)
+        for i, module in enumerate(linear_layers):
+            d_s_l = layer_grads[module]
+            grad_cov = torch.einsum("...ti,...tj->tij", d_s_l, d_s_l)
+            # TODO 
+            # RuntimeError: The size of tensor a (24) must match the size of tensor b (8) at non-singleton dimension 1
+            kfac_grad_covs[i] += grad_cov.mean(dim=0)
+            
+            # gradient of the loss wrt the weights
+            w_grad = module.weight.grad
+            b_grad = module.bias.grad.unsqueeze(-1)
+            full_grad = torch.cat([w_grad, b_grad], dim=-1)
+            grads[i].append(full_grad)
 
         tot+=1
 
@@ -107,17 +105,49 @@ def compute_ekfac_factors(model: nn.Module, train_dataset: Dataset):
 
     return kfac_input_covs, kfac_grad_covs, grads
 
-compute_ekfac_factors(model, train_dataset)
-
 # 3. get search grads
 # 
 # TODO difference between psuedo grads and serach grads
 
-def compute_grads():
+def compute_grads(model: nn.Module, train_dataset: Dataset):
+    grads = [[] for _ in range(len(linear_layers))]
 
-    return
+    for X, Y in train_dataset: 
+        model.zero_grad()
+
+    x_ids = X['input_ids'].to(device)
+    y_ids = Y['input_ids'].to(device)
+
+    if len(x_ids.shape) == 1:
+        x_ids = x_ids.unsqueeze(0)
+        y_ids = y_ids.unsqueeze(0)
+
+    logits, loss = model(x_ids, y_ids)
+
+    loss.backward()
+
+    for i, module in enumerate(linear_layers):
+        w_grad = module.weight.grad
+        b_grad = module.bias.grad.unsqueeze(-1)
+        full_grad = torch.cat([w_grad, b_grad], dim=-1)
+        grads[i].append(full_grad)
+
+    return grads
 
 # 4. compute ekfac inverse hessian vector product
+
+def compute_ekfac_ihvp(kfac_input_covs, kfac_grad_covs, pseudo_grads, computed_grads, damping=0.001):
+    ihvp = []
+
+kfac_input_cov, kfac_grad_cov, pseudo_grads = compute_ekfac_factors(model, train_dataset)
+
+# TODO reread paper to understand this
+computed_grads = compute_grads(model, train_dataset)
+
+
+
+
+
 
 # 5. for each test point:
 # get gradient of test point wrt loss?
